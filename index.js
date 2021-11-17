2158c8130cb8583933dcb8f18ae15f9a14c98a79
pfreisleben <pedrofreislebeen@gmail.com>
2021-11-17 20:33:05 -0300

    ok

--- index.js
+++ index.js
@@ -13,8 +13,20 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Api Funcionando!' });
 });
 
-const cidadesRouter = require('./routers/cidades.routes');
-app.use('/cidades', cidadesRouter);
+const acaoRouter = require('./routers/acao.routes');
+app.use('/acao', acaoRouter);
+
+const corridaRouter = require('./routers/corrida.routes');
+app.use('/corrida', corridaRouter);
+
+const estrategiaRouter = require('./routers/estrategia.routes');
+app.use('/estrategia', estrategiaRouter);
+
+const futebolRouter = require('./routers/futebol.routes');
+app.use('/futebol', futebolRouter);
+
+const terrorRouter = require('./routers/terror.routes');
+app.use('/terror', terrorRouter);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em: http://localhost:${port}`);
